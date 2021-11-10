489af8ca2e764ab013bec782d15d4afaef12b797
Lucas Briese <jackjasonjones+github@gmail.com>
2021-11-10 20:01:14 +0100

    added iproute2 to remoive warnings

--- scripts/installMinimalDependencies.sh
+++ scripts/installMinimalDependencies.sh
@@ -5,7 +5,9 @@ set -o nounset
 echo "[installMinimalDependencies] installing ..."
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ca-certificates wget locales curl
+# curl / wget needed for lgsm
+# iproute2, fix "-o: command not found", fix "ss: command not found"
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends ca-certificates wget locales curl iproute2
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
 # verify gosu is working
