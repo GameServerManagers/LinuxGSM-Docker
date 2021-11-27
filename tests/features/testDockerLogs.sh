02a7c105db9b5b2c431901c36d01c7ab37532e8a
Lucas Briese <jackjasonjones+github@gmail.com>
2021-11-27 04:33:34 +0100

    fix rename issues

--- tests/features/testDockerLogs.sh
+++ tests/features/testDockerLogs.sh
@@ -9,8 +9,9 @@ GAMESERVER="$2"
 VOLUME="$3"
 
 (
+    log="$(realpath "$(dirname "$0")")/testDockerLogs.log"
     cd "$(dirname "$0")/../.."
-    log="tests/functions/testDockerLogs.log"
+    
     ./tests/quick.sh --quick --logs --version "$VERSION" --volume "$VOLUME" "$GAMESERVER" > "$log"
     if grep -qE 'VAC\s*secure\s*mode\s*is\s*activated.' "$log"; then
         rm "$log"
