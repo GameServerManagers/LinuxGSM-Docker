e2b9bfd4d152babf8e46e21bb9c53f8b94785f04
Lucas Briese <lbriese@mail.upb.de>
2022-02-12 11:26:58 +0100

    + Flaky Test

--- tests/full.sh
+++ tests/full.sh
@@ -8,6 +8,7 @@ ROOT_FOLDER="$(realpath "$(dirname "$0")/..")"
 
 PARRALEL="$(lscpu -p | grep -Ev '^#' | sort -u -t, -k 2,4 | wc -l)"
 IMAGE="gameservermanagers/linuxgsm-docker"
+FLAKY="1"
 RERUN="false"
 SUFFIX=""
 VOLUMES="false"
@@ -25,6 +26,7 @@ while [ $# -ge 1 ]; do
             echo "[help][full] options:"
             echo "[help][full] -c --cpus    x  run x servers in parralel, default x = physical cores"
             echo "[help][full]    --image   x  set target image"
+			echo "[help][full]    --flaky   x  test for flaky results x times, x should be greater as 1"
             echo "[help][full]    --rerun      check results and runs every gameserver which wasn't successful"
             echo "[help][full]    --suffix     suffix to add to every image"
             echo "[help][full]    --volumes    use volumes \"linuxgsm-SERVERCODE\""
@@ -41,6 +43,9 @@ while [ $# -ge 1 ]; do
         --image)
             IMAGE="$1"
             shift;;
+		--flaky)
+			FLAKY="$1"
+			shift;;
         --rerun)
             RERUN="true";;
         --suffix)
@@ -70,100 +75,138 @@ fi
 # shellcheck source=tests/internal/api_various.sh
 source "$(dirname "$0")/internal/api_various.sh"
 
-# prepare results folder
-RESULTS="$ROOT_FOLDER/tests/results"
-if [ "${#GAMESERVER[@]}" = "0" ]; then
-    if "$RERUN"; then
-        find "$RESULTS" -type f ! -name "successful.*" -exec rm -f "{}" \;
-    else
-        rm -rf "$RESULTS"
-    fi
-else
-    # rerun only remove specific log
-    for servercode in "${GAMESERVER[@]}"; do
-        rm -rf "${RESULTS:?}/"*".$servercode.log"
-    done
+for run in $(seq 1 "$FLAKY"); do
+	# prepare results folder
+	RESULTS="$ROOT_FOLDER/tests/results"
+	# for multiple runs move previous results folder
+	if [ "$FLAKY" != "1" ] && [ "$run" -gt "1" ]; then
+		rm -rf "$RESULTS.$((run-1))" > /dev/null 2>&1
+		mv -f "$RESULTS" "$RESULTS.$((run-1))"
+	fi
+	if [ "${#GAMESERVER[@]}" = "0" ]; then
+		if "$RERUN"; then
+			find "$RESULTS" -type f ! -name "successful.*" -exec rm -f "{}" \;
+		else
+			rm -rf "$RESULTS"
+		fi
+	else
+		# rerun only remove specific log
+		for servercode in "${GAMESERVER[@]}"; do
+			rm -rf "${RESULTS:?}/"*".$servercode.log"
+		done
+	fi
+	mkdir -p "$RESULTS"
+
+	(
+		if "$RERUN"; then
+			echo "[info][full] building linuxgsm base once"
+			./tests/internal/build.sh --version "$VERSION" --image "$IMAGE" --latest --suffix "$SUFFIX"
+		else
+			echo "[info][full] skipping building linuxgsm because rerun"
+		fi
+
+		subprocesses=()
+		function handleInterrupt() {
+			for pid in "${subprocesses[@]}"; do
+				kill -s SIGINT "$pid" || true
+			done
+		}
+		trap handleInterrupt SIGTERM SIGINT
+
+		mapfile -d $'\n' -t servers < <(getServerCodeList "$VERSION")
+		for server_code in "${servers[@]}"; do
+			cd "$ROOT_FOLDER"
+
+			# only start $PARRALEL amount of tests
+			while [ "${#subprocesses[@]}" -ge "$PARRALEL" ]; do
+				sleep 1s
+				temp=()
+				for pid in "${subprocesses[@]}"; do
+					if ps -p "$pid" -o pid= > /dev/null 2>&1; then
+						temp+=("$pid")
+					fi
+				done
+				subprocesses=("${temp[@]}")
+			done
+
+
+			isServercodeInServerlist="$(grep -qF "$server_code" <<< "${GAMESERVER[@]}" && echo true || echo false )"
+			serverDidntStartSuccessful="$([ ! -f "$RESULTS/successful.$server_code.log" ] && echo true || echo false )"
+			testThisServercode="$( ("$testAllServer" || "$isServercodeInServerlist") && echo true || echo false )"
+			rerunIsFine="$( ( ! "$RERUN" || "$serverDidntStartSuccessful" ) && echo true || echo false )"
+			if "$testThisServercode" && "$rerunIsFine"; then
+				echo "[info][full] testing: $server_code"
+				(
+					quick=(./tests/quick.sh --logs --version "$VERSION" --image "$IMAGE" --skip-lgsm --suffix "$SUFFIX")
+					if "$VOLUMES"; then
+						quick+=(--volume "linuxgsm-$server_code")
+					fi
+					quick+=("$server_code")
+
+					echo "${quick[@]}"
+					if "${quick[@]}" > "$RESULTS/$server_code.log" 2>&1; then
+						mv "$RESULTS/$server_code.log" "$RESULTS/successful.$server_code.log"
+					else
+						mv "$RESULTS/$server_code.log" "$RESULTS/failed.$server_code.log"
+					fi
+				) | tee /dev/tty > /dev/null 2>&1 &
+				subprocesses+=("$!")
+			fi
+		done
+
+		# await every job is done
+		while [ "${#subprocesses[@]}" -gt "0" ]; do
+			sleep 1s
+			temp=()
+			for pid in "${subprocesses[@]}"; do
+				if ps -p "$pid" -o pid= > /dev/null 2>&1; then
+					temp+=("$pid")
+				fi
+			done
+			subprocesses=("${temp[@]}")
+		done
+
+		echo "[info][full] successful: $(find "$RESULTS/" -iname "successful.*" | wc -l)"
+		echo "[info][full] failed: $(find "$RESULTS/" -iname "failed.*" | wc -l)"
+
+		mapfile -t failed_credentials_missing < <(grep --include "*failed*" -rlF 'Change steamuser="username"' "$RESULTS" | sort | uniq || true)
+		echo "[info][full] failed - unset steam credentials: $(grep -Po '(?<=failed.)[^.]*' <<< "${failed_credentials_missing[@]}" | tr '\n' ' ' || true)"
+		# print filenames + very high line number to jump right at eof on click if IDE supports it
+		printf '%s\n' "${failed_credentials_missing[@]/%/:100000}"
+
+		mapfile -t failed_other < <(grep --include "*failed*" -rLF 'Change steamuser="username"' "$RESULTS" | sort | uniq || true)
+		echo "[info][full] failed - other error: $(grep -Po '(?<=failed.)[^.]*' <<< "${failed_other[@]}" | tr '\n' ' ' || true)"
+		printf '%s\n' "${failed_other[@]/%/:100000}"
+	)
+done
+
+if [ "$FLAKY" != "1" ]; then
+	mapfile -t results_folder < <(find "$ROOT_FOLDER" -type d -iname "results.*")
+
+	mapfile -t servercodes < <(find "$RESULTS" -type f -iname "*.log" | grep -Poe '(?<=.)[^./]+(?=.log)' | sort)
+	for servercode in "${servercodes[@]}"; do
+		successful=()
+		failed=()
+		if [ -f "$RESULTS/failed.$servercode.log" ]; then
+			failed=("$RESULTS/failed.$servercode.log")
+		elif [ -f "$RESULTS/successful.$servercode.log" ]; then
+			successful+=("$RESULTS/successful.$servercode.log")
+		fi
+
+		for result_folder in "${results_folder[@]}"; do
+			if [ -f "$result_folder/successful.$servercode.log" ]; then
+				successful+=("$result_folder/successful.$servercode.log")
+			elif [ -f "$result_folder/failed.$servercode.log" ]; then
+				failed+=("$result_folder/failed.$servercode.log")
+			fi
+		done
+
+		if [ "${#successful[@]}" -gt "0" ] && [ "${#failed[@]}" -gt "0" ]; then
+			echo ""
+			echo "$servercode flaky result"
+			for result in "${successful[@]}" "${failed[@]}"; do
+				echo "$result:10000"
+			done
+		fi
+	done
 fi
-mkdir -p "$RESULTS"
-
-(
-    if "$RERUN"; then
-        echo "[info][full] building linuxgsm base once"
-        ./tests/internal/build.sh --version "$VERSION" --image "$IMAGE" --latest --suffix "$SUFFIX"
-    else
-        echo "[info][full] skipping building linuxgsm because rerun"
-    fi
-
-    subprocesses=()
-    function handleInterrupt() {
-        for pid in "${subprocesses[@]}"; do
-            kill -s SIGINT "$pid" || true
-        done
-    }
-    trap handleInterrupt SIGTERM SIGINT
-
-    mapfile -d $'\n' -t servers < <(getServerCodeList "$VERSION")
-    for server_code in "${servers[@]}"; do
-        cd "$ROOT_FOLDER"
-
-        # only start $PARRALEL amount of tests
-        while [ "${#subprocesses[@]}" -ge "$PARRALEL" ]; do
-            sleep 1s
-            temp=()
-            for pid in "${subprocesses[@]}"; do
-                if ps -p "$pid" -o pid= > /dev/null 2>&1; then 
-                    temp+=("$pid")
-                fi
-            done
-            subprocesses=("${temp[@]}")
-        done
-        
-        
-        isServercodeInServerlist="$(grep -qF "$server_code" <<< "${GAMESERVER[@]}" && echo true || echo false )"
-        serverDidntStartSuccessful="$([ ! -f "$RESULTS/successful.$server_code.log" ] && echo true || echo false )"
-        testThisServercode="$( ("$testAllServer" || "$isServercodeInServerlist") && echo true || echo false )"
-        rerunIsFine="$( ( ! "$RERUN" || "$serverDidntStartSuccessful" ) && echo true || echo false )"
-        if "$testThisServercode" && "$rerunIsFine"; then
-            echo "[info][full] testing: $server_code"
-            (   
-                quick=(./tests/quick.sh --logs --version "$VERSION" --image "$IMAGE" --skip-lgsm --suffix "$SUFFIX")
-                if "$VOLUMES"; then
-                    quick+=(--volume "linuxgsm-$server_code")
-                fi
-                quick+=("$server_code")
-
-                echo "${quick[@]}"
-                if "${quick[@]}" > "$RESULTS/$server_code.log" 2>&1; then
-                    mv "$RESULTS/$server_code.log" "$RESULTS/successful.$server_code.log"
-                else
-                    mv "$RESULTS/$server_code.log" "$RESULTS/failed.$server_code.log"
-                fi
-            ) | tee /dev/tty > /dev/null 2>&1 &
-            subprocesses+=("$!")
-        fi
-    done
-
-    # await every job is done
-    while [ "${#subprocesses[@]}" -gt "0" ]; do
-        sleep 1s
-        temp=()
-        for pid in "${subprocesses[@]}"; do
-            if ps -p "$pid" -o pid= > /dev/null 2>&1; then 
-                temp+=("$pid")
-            fi
-        done
-        subprocesses=("${temp[@]}")
-    done
-
-    echo "[info][full] successful: $(find "$RESULTS/" -iname "successful.*" | wc -l)"
-    echo "[info][full] failed: $(find "$RESULTS/" -iname "failed.*" | wc -l)"
-
-    mapfile -t failed_credentials_missing < <(grep --include "*failed*" -rlF 'Change steamuser="username"' "$RESULTS" | sort | uniq || true)
-    echo "[info][full] failed - unset steam credentials: $(grep -Po '(?<=failed.)[^.]*' <<< "${failed_credentials_missing[@]}" | tr '\n' ' ' || true)"
-    # print filenames + very high line number to jump right at eof on click if IDE supports it
-    printf '%s\n' "${failed_credentials_missing[@]/%/:100000}"
-
-    mapfile -t failed_other < <(grep --include "*failed*" -rLF 'Change steamuser="username"' "$RESULTS" | sort | uniq || true)
-    echo "[info][full] failed - other error: $(grep -Po '(?<=failed.)[^.]*' <<< "${failed_other[@]}" | tr '\n' ' ' || true)"
-    printf '%s\n' "${failed_other[@]/%/:100000}"
-)
