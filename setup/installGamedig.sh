2f9f66b3a4173a72c7e83c3847449d715b527625
Lucas Briese <lbriese@mail.upb.de>
2022-02-21 17:44:24 +0100

    fix gamedig not found

--- setup/installGamedig.sh
+++ setup/installGamedig.sh
@@ -10,17 +10,24 @@ echo 'tzdata tzdata/Areas select Europe' | debconf-set-selections
 echo 'tzdata tzdata/Zones/Europe select Berlin' | debconf-set-selections
 
 apt-get update
-DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends xz-utils
+DEBIAN_FRONTEND="noninteractive" apt-get install -y --no-install-recommends xz-utils jq
 
-NODE_VERSION=v16.14.0
+NODE_VERSION="v16.14.0"
 wget "https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz"
 mkdir -p "/usr/local/lib/nodejs"
 tar -xJvf "node-$NODE_VERSION-linux-x64.tar.xz" -C "/usr/local/lib/nodejs"
-printf '\nexport PATH="%s:/usr/local/lib/nodejs/node-%s-linux-x64/bin"\n' "$PATH" "$NODE_VERSION" > ~/.bashrc
+rm "node-$NODE_VERSION-linux-x64.tar.xz"
 
-. ~/.profile
+mapfile -t npm_bin < <(find "/usr/local/lib/nodejs/node-$NODE_VERSION-linux-x64/bin/" ! -type d -exec basename "{}" \;)
+for bin in "${npm_bin[@]}"; do
+    ln -vs "/usr/local/lib/nodejs/node-$NODE_VERSION-linux-x64/bin/$bin" "/usr/local/bin/$bin"
+done
 
 node -v
 npm version
 npx -v
 npm install -g gamedig
+ln -vs "/usr/local/lib/nodejs/node-$NODE_VERSION-linux-x64/bin/gamedig" "/usr/local/bin/gamedig"
+
+command -v gamedig
+command -v jq
