c3660d72f211b21eb7a3f01cbd62085a659c88b3
Lucas Briese <lbriese@mail.upb.de>
2022-04-10 03:38:14 +0200

    fix: openssl x86 instead of x64 for svenserver

--- setup/installOpenSSL_1.1n.sh
+++ setup/installOpenSSL_1.1n.sh
@@ -2,22 +2,13 @@
 
 set -euo pipefail
 version="1.1.1n"
-package="1748639999ed79b998e4fe4a6d292ed8e874736a"
 
 apt-get update
-apt-get install -y python3-pip
-pip3 install conan
+apt-get install -y python3-pip gcc-multilib
+pip3 install --force-reinstall conan
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
-conan download -p "$package" "openssl/$version@_/_"
-
-(
-    cd ~/".conan/data/openssl/$version/_/_/package/$package/lib/"
-    cp *.so "/usr/local/lib"
-    cd "/usr/local/lib"
-    for file in *.so; do
-        mv "$file" "$file.1.1"
-    done
-)
+conan install -if "$(mktemp -d)" -s arch=x86 -s arch_build=x86 --build=missing -o openssl:shared=True "openssl/$version@_/_"
+cp -vf ~/.conan/data/openssl/"$version"/_/_/package/*/lib/*.so.1.1 "/usr/local/lib"
 # reindex with: ldconfig /usr/local/lib
 # ldd *.so should be fine and ldconfig -p should list the so
