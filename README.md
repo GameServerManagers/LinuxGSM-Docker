855b535757922512f7938e3c3f9a3744ffc06895
Lucas Briese <lbriese@mail.upb.de>
2022-07-17 16:57:43 +0200

    doc: compose examples autogenerated

--- README.md
+++ README.md
@@ -13,52 +13,10 @@ Dockerhub https://hub.docker.com/r/gameservermanagers/linuxgsm-docker/
 
 ## How to use:
 
-1. There are prebuild images, if you think you need to build it yourself have a look at [testing readme](./test/testing.md)
+1. Choose your servercode and replace in below
+2. start `docker-compose -f examples/SERVER.yml up -d`
+3. get config location `docker exec -it lgsm-SERVER-1 details`
+4. copy your own config into: `docker cp my_local_config.txt lgsm-SERVER-1:/config_location_from_step_3`
+5. print config content `docker exec lgsm-SERVER-1 cat /config_location_from_step_3`
 
 
-
-  
-- `/tests`
-  - `steam_test_credentials` some servers need credentials here, currently steam guard is not supported
-  - `single.sh` test a single servercode
-  - `multiple.sh` test multiple servercodes at once
-
-
-
-
-
-1. Pick your servercode e.g. `rustserver`
-2. Pick ports needed
-## Create Persistant Storage
-Game servers require persistant storage to store the server files and configs. The docker reccomended way is to use Persistant storage
-
-```
-docker volume create csgoserver
-```
-
-# Install and Start Game server
-```
-docker run -d --name csgoserver -v csgoserver --net-host -it -env GAMESERVERNAME=csgoserver gameservermanagers/linuxgsm-docker
-```
-# Run LinuxGSM commands
-Commands can be run just like standard LinuxGSM using the docker exec command.
-```
-docker exec -it csgoserver ./csgoserver details
-```
-# Edit LinuxGSM config
-To edit the LinuxGSM config files use the following.
-
-View the default settings
-```
-docker exec -it csgoserver nano _default.cfg
-```
-Edit LinuxGSM config settings
-```
-docker exec -it csgoserver nano common.cfg
-```
-Edit Game Server Config settings
-To edit the game server config settings run the following.
-
-```
-docker exec -it csgoserver nano server.cfg
-```
---
+++ deploy/generateExamples.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+version="$1"
+
+(
+    cd "$(dirname "$0")/.."
+
+    source "test/internal/api_various.sh"
+    source "test/internal/api_docker.sh"
+    image="$DEFAULT_DOCKER_REPOSITORY"
+
+    mapfile -d $'\n' -t servers < <(getServerCodeList "$version")
+    for server in "${servers[@]}"; do
+        echo "starting $server"
+        container="$server-generateExamples"
+        docker stop "$container" > /dev/null 2>&1 || true
+        docker rm "$container" > /dev/null 2>&1 || true
+
+        # run it once
+        run=(./test/internal/run.sh --container "$container" --image "$image" --tag "$server" --detach)
+        # assuming already build before? Else takes way more time.
+        #if ! ./test/single.sh --build-only --version "$version" --image "$image" "$server"; then
+        #  echo "build failed, skipping"
+        #  continue
+        #fi
+        echo "${run[@]}"
+        if ! "${run[@]}" > /dev/null; then
+            echo "run failed, skipping"
+            continue
+        fi
+
+        seconds=0
+        echo -n "[info][awaitContainerStarted] waiting for $seconds"
+        while docker exec "$container" ls > /dev/null 2>&1 && ! docker exec "$container" ls "$server" > /dev/null 2>&1; do
+          sleep 1s # just waiting until linuxgsm is installed
+          seconds=$((seconds+1))
+          echo -en "\r[info][awaitContainerStarted] waiting for $seconds"
+        done
+        echo -e "\r[info][awaitContainerStarted] waited for $seconds "
+
+        # get all ports
+        details="$(docker exec -it "$container" details 2>&1)"
+
+        docker stop "$container" > /dev/null 2>&1 || true
+        docker rm "$container" > /dev/null 2>&1 || true
+
+        # write docker-compose.yml
+        compose_file="
+# usage:
+#  docker-compose -f ./examples/$server.yml up -d
+volumes:
+  serverfiles:
+
+name: lgsm-$server
+
+services:
+  service:
+    image: \"$image:$server\"
+    tty: true
+    restart: unless-stopped
+    environment:
+      - \"CRON_update_daily=0 7 * * *\" 
+    volumes:
+      - serverfiles:/home/linuxgsm
+      - /etc/localtime:/etc/localtime:ro"
+        echo "$compose_file" > "examples/$server.yml"
+
+        # extract port info
+        mapfile -t ports < <(echo "$details" | grep -E '^[a-zA-Z0-9_-]+\s+[0-9]+\s+[a-z]+\s+\S+')
+        if [ "${#ports[@]}" -gt 0 ]; then
+            echo "    ports:" >> "examples/$server.yml"
+
+            # we have ports but maybe they are duplicate
+            # => we need to merge the desc
+            already_added=""
+            for port_i in "${ports[@]}"; do
+                desc="$(echo "$port_i" | awk '{ print $1 }')"
+                value="$(echo "$port_i" | awk '{ print $2 }')"
+                protocol="$(echo "$port_i" | awk '{ print $3 }')"
+
+                merged_desc="$desc"
+                for port_j in "${ports[@]}"; do
+                  desc_j="$(echo "$port_j" | awk '{ print $1 }')"
+                  value_j="$(echo "$port_j" | awk '{ print $2 }')"
+                  protocol_j="$(echo "$port_j" | awk '{ print $3 }')"
+
+                  # port/proto is identical
+                  if [ "$value:$protocol" = "$value_j:$protocol_j" ] && ! grep -q "$desc_j" <<< "$merged_desc"; then
+                    merged_desc="$merged_desc / $desc_j"
+                  fi
+                done
+
+                if ! grep -q " $value/$protocol " <<< "$already_added"; then
+                  already_added="$already_added $value/$protocol "
+                  echo "      # $merged_desc" >> "examples/$server.yml"
+                  echo "      - \"$value:$value/$protocol\"" >> "examples/$server.yml"
+                fi
+            done
+        else
+            echo "# couldn't extract ports, can only provide example how to configure it" >> "examples/$server.yml"
+            echo "#    ports:" >> "examples/$server.yml"
+            echo "#      - \"27015:27015/udp\"" >> "examples/$server.yml"
+            echo "#      - \"27015:27015/tcp\"" >> "examples/$server.yml"
+            
+        fi
+        if grep -q "b" <<< "$server"; then
+          exit 0
+        fi
+    done
+)
